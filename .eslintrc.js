9eba4e6bdf5e2a6c87d7681e320837da3fa690c2
Victor Mazo <viamazoba@unal.edu.co>
2025-03-15 15:16:56 -0500

    feat: Se implementa sección de lista y se actualizan reglas del eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['@react-native', 'plugin:react/jsx-runtime'],
   rules: {
     'react-native/no-inline-styles': 'off',
+    'react/no-unstable-nested-components': 'off',
   },
 };
