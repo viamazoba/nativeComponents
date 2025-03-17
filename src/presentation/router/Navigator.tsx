ab4a57b6cf1e413655c2f6e6c7581a3f5b5d0d6a
Victor Mazo <viamazoba@unal.edu.co>
2025-03-16 20:23:52 -0500

    feat: Se inicia la implementación de theme dinámico

--- src/presentation/router/Navigator.tsx
+++ src/presentation/router/Navigator.tsx
@@ -10,6 +10,7 @@ import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
 import { ModalScreen } from '../screens/ui/ModalScreen';
 import { InfiniteScrollScreen } from '../screens/ui/InfiniteScrollScreen';
 import { SlidesScreen } from '../screens/ui/SlidesScreen';
+import { ChangeThemeScreen } from '../screens/theme/ChangeThemeScreen';
 
 
 const Stack = createStackNavigator();
@@ -31,6 +32,7 @@ export const Navigator = () => {
             <Stack.Screen name="ModalScreen" component={ModalScreen} />
             <Stack.Screen name="InfiniteScrollScreen" component={InfiniteScrollScreen} />
             <Stack.Screen name="SlidesScreen" component={SlidesScreen} />
+            <Stack.Screen name="ChangeThemeScreen" component={ChangeThemeScreen} />
         </Stack.Navigator>
     );
 };
---
+++ src/presentation/screens/theme/ChangeThemeScreen.tsx
@@ -0,0 +1,26 @@
+import { View } from 'react-native';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { Button } from '../../components/ui/Button';
+
+
+export const ChangeThemeScreen = () => {
+
+    return (
+        <CustomView margin>
+            <Title text="Cambiar tema" safe />
+
+            <Button
+                text="Light"
+                onPress={() => { }}
+            />
+
+            <View style={{ height: 10 }} />
+
+            <Button
+                text="Dark"
+                onPress={() => { }}
+            />
+        </CustomView>
+    );
+};
